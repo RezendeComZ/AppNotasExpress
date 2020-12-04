8f648d6dd1a810cb330573b992b78ecd95545138
Gabriel Rezende <g@brielrezen.de>
2020-12-04 19:58:54 -0300

    feat: controller adicionado

---
+++ controllers/controle.js
@@ -0,0 +1,25 @@
+let notas = require('../notasSalvas')
+
+const controle = {
+  home: function(req, res, next) {
+    let fixos = notas.filter(nota => nota.pin === true)
+    let naoFixos = notas.filter(nota => nota.pin === false)
+    res.render('index', { fixos: fixos, naoFixos: naoFixos });
+  },
+  mostraNota: (req, res,) => {
+    res.render('_form');
+  },
+  redictHome: (req, res) => {
+    res.redirect('/');
+  },
+  notaIndividual: (req, res) => {
+    let reg = notas.findIndex( element => element.id === parseInt(req.params.id))
+    if (reg > -1) {
+      res.render('nota', { id:req.params.id, nota: notas[reg] });
+    } else {
+      res.send('Nota inexistente')
+    }
+  }
+}
+
+module.exports = controle;
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,18 @@
 var express = require('express');
 var router = express.Router();
 let notas = require('../notasSalvas')
+const controle = require('../controllers/controle')
 
 // Home
-router.get('/', function(req, res, next) {
-  let fixos = notas.filter(nota => nota.pin === true)
-  let naoFixos = notas.filter(nota => nota.pin === false)
-  res.render('index', { fixos: fixos, naoFixos: naoFixos });
-});
+router.get('/', controle.home);
 
 // Página de nota indidual
-router.get('/nota/:id', (req, res) => {
-  let reg = notas.findIndex( element => element.id === parseInt(req.params.id))
-  if (reg > -1) {
-    res.render('nota', { id:req.params.id, nota: notas[reg] });
-  } else {
-    res.send('Nota inexistente')
-  }
-});
+router.get('/nota/:id', controle.notaIndividual);
 
 // Form - incompleto
-router.get('/form', function(req, res, next) {
-  res.render('_form');
-});
+router.get('/form', controle.mostraNota);
 
+// Redirectionar
+router.get('/notas', controle.redictHome)
 
 module.exports = router;

4ea1341ada2a6b68836810b3caf5161f630fc12c
Gabriel Rezende <g@brielrezen.de>
2020-12-07 10:10:49 -0300

    moongoose configurado, mas ainda falta a integração

---
+++ models/modelo.js
@@ -0,0 +1,27 @@
+const mongoose = require('../node_modules/mongoose')
+const Schema =  mongoose.Schema;
+
+const notaSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  h: {
+    type: String,
+    required: true
+  },
+  b: {
+    type: String,
+    required: false
+  },
+  pin: {
+    type: Boolean,
+    required: false
+  }
+
+}, { timestamps: true });
+
+
+const Nota = mongoose.model('Nota', notaSchema) // Vai acessar a coleção na DB
+
+module.exports = Nota;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const controle = require('../controllers/controle')
+const Nota = require('../models/modelo');
+
 
 // Home
 router.get('/', controle.home);
@@ -15,3 +17,45 @@ router.get('/form', controle.mostraNota);
 router.get('/notas', controle.redictHome)
 
 module.exports = router;
+
+
+// mongoose e mongo sandbox routes:
+router.get('/add-nota', (req, res) => {
+  const notaTeste = new Nota({
+    id: 69,
+    h: 'Testando hidusagduahgdasjhg',
+    b: 'body que fica na nota',
+    pin: true
+  });
+  // Método pra salvar no banco de dados (assíncrona):
+  notaTeste.save()
+  .then((result) => {
+    res.send(result)
+    console.log('Aparentemente foi pra DB (index em routes)')
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
+
+// Mostrar o que tem no banco de dados:
+router.get('/todas-notas', (req, res) => {
+  Nota.find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+// Mostrar uma nota
+router.get('/uma-nota', (req, res) => {
+  Nota.findById('5fcd52e100b2cda9e2e75994')
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
